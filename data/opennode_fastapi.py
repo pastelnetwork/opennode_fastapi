352e37b8a2073d3547ab21f6325c2a8fde1465cc
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-01-28 22:51:26 -0500

    Add files via upload

--- data/opennode_fastapi.py
+++ data/opennode_fastapi.py
@@ -20,10 +20,6 @@ class OpenNodeFastAPIRequests(SqlAlchemyBase):
     valid_request: bool = sa.Column(sa.Boolean, default=True)
     operation_status_json: str = sa.Column(sa.String, nullable=True)
 
-    # User relationship
-    user_id: int = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
-    user = orm.relationship('User')
-
     @property
     def __repr__(self):
         return '<OpenNodeFastAPIRequests {}>'.format(self.id)
@@ -35,3 +31,43 @@ class ValidationError(Exception):
 
         self.status_code = status_code
         self.error_msg = error_msg
+        
+class OpenAPISenseData(SqlAlchemyBase):
+    __tablename__ = 'openapi_sense_data'
+    sense_registration_ticket_txid: str = sa.Column(sa.String, nullable=False, index=True, primary_key=True)
+    hash_of_candidate_image_file: str = sa.Column(sa.String, nullable=False, index=True)
+    pastel_id_of_submitter: str = sa.Column(sa.String, nullable=False, index=True)
+    pastel_block_hash_when_request_submitted: str = sa.Column(sa.String, nullable=False)
+    pastel_block_height_when_request_submitted: str = sa.Column(sa.String, nullable=False)
+    dupe_detection_system_version: str = sa.Column(sa.String, nullable=False)
+    overall_rareness_score: float = sa.Column(sa.Float, nullable=False)
+    open_nsfw_score: float = sa.Column(sa.Float, nullable=False)
+    alternative_nsfw_scores: str = sa.Column(sa.String, nullable=False)
+    utc_timestamp_when_request_submitted: str = sa.Column(sa.String, nullable=False)
+    is_likely_dupe: str = sa.Column(sa.String, nullable=False)
+    is_rare_on_internet: str = sa.Column(sa.String, nullable=False)
+    is_pastel_openapi_request: str = sa.Column(sa.String, nullable=False)
+    image_fingerprint_of_candidate_image_file: str = sa.Column(sa.String, nullable=False)
+    pct_of_top_10_most_similar_with_dupe_prob_above_25pct: float = sa.Column(sa.Float, nullable=False)
+    pct_of_top_10_most_similar_with_dupe_prob_above_33pct: float = sa.Column(sa.Float, nullable=False)
+    pct_of_top_10_most_similar_with_dupe_prob_above_50pct: float = sa.Column(sa.Float, nullable=False)
+    internet_rareness__min_number_of_exact_matches_in_page: str = sa.Column(sa.String, nullable=False)
+    internet_rareness__earliest_available_date_of_internet_results: str = sa.Column(sa.String, nullable=False)
+    internet_rareness__b64_image_strings_of_in_page_matches: str = sa.Column(sa.String, nullable=False)
+    internet_rareness__original_urls_of_in_page_matches: str = sa.Column(sa.String, nullable=False)
+    internet_rareness__result_titles_of_in_page_matches: str = sa.Column(sa.String, nullable=False)
+    internet_rareness__date_strings_of_in_page_matches: str = sa.Column(sa.String, nullable=False)
+    alternative_rare_on_internet__number_of_similar_results: str = sa.Column(sa.String, nullable=False)
+    alternative_rare_on_internet__b64_image_strings: str = sa.Column(sa.String, nullable=False)
+    alternative_rare_on_internet__original_urls: str = sa.Column(sa.String, nullable=False)
+    alternative_rare_on_internet__result_titles: str = sa.Column(sa.String, nullable=False)
+    
+      
+class OpenAPIRawSenseData(SqlAlchemyBase):
+    __tablename__ = 'openapi_raw_sense_data'
+    sense_registration_ticket_txid: str = sa.Column(sa.String, nullable=False, index=True, primary_key=True)
+    hash_of_candidate_image_file: str = sa.Column(sa.String, nullable=False, index=True)
+    pastel_id_of_submitter: str = sa.Column(sa.String, nullable=False, index=True)
+    pastel_block_hash_when_request_submitted: str = sa.Column(sa.String, nullable=False)
+    pastel_block_height_when_request_submitted: str = sa.Column(sa.String, nullable=False)
+    raw_sense_data_json: str = sa.Column(sa.String, nullable=False)
