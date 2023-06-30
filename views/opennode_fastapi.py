e2911a422bf92de54df564fd497461d761df1cc3
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-06-30 16:38:01 -0400

    Update opennode_fastapi.py

--- views/opennode_fastapi.py
+++ views/opennode_fastapi.py
@@ -7,20 +7,21 @@ from typing import Union, Dict, Any
 
 import fastapi
 from fastapi import BackgroundTasks, Depends, Response, Query
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi.responses import JSONResponse, StreamingResponse, HTMLResponse
 from fastapi_chameleon import template
 from starlette.requests import Request
 from starlette import status
 
 from viewmodels.opennode_fastapi.data_viewmodel import DataViewModel
-from data.opennode_fastapi import OpenNodeFastAPIRequests, ValidationError
+from data.opennode_fastapi import OpenNodeFastAPIRequests, ValidationError, ShowLogsIncrementalModel, LogLines
 from services import opennode_fastapi_service
 from fastapi import BackgroundTasks
 from json import JSONEncoder
-import datetime
+from datetime import datetime, timedelta
 import aiohttp
 import asyncio
 import httpx
+from pytz import timezone
 
 
 router = fastapi.APIRouter()
@@ -824,3 +825,196 @@ async def run_bulk_test_cascade(num_downloads: int = Query(5, description="Numbe
         return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
     except Exception as x:
         return fastapi.Response(content=str(x), status_code=500)
+
+
+@router.get("/show_logs_incremental/{minutes}/{last_position}", response_model=ShowLogsIncrementalModel)
+def show_logs_incremental(minutes: int, last_position: int):
+    new_logs = []
+    now = datetime.now(timezone('UTC'))  # get current time, make it timezone-aware
+    try:
+        with open("opennode_fastapi_log.txt", "r") as f:
+            f.seek(last_position)  # seek to `last_position`
+            while True:
+                line = f.readline()
+                if line == "":  # if EOF
+                    break
+                if line.strip() == "":
+                    continue
+                try:  # Try to parse the datetime
+                    log_datetime_str = line.split(" - ")[0]  # assuming the datetime is at the start of each line
+                    log_datetime = datetime.strptime(log_datetime_str, "%Y-%m-%d %H:%M:%S,%f")  # parse the datetime string to a datetime object
+                    log_datetime = log_datetime.replace(tzinfo=timezone('UTC'))  # set the datetime object timezone to UTC to match `now`
+                    if now - log_datetime > timedelta(minutes=minutes):  # if the log is older than `minutes` minutes from now
+                        continue  # ignore the log and continue with the next line
+                except ValueError:
+                    pass  # If the line does not start with a datetime, ignore the ValueError and process the line anyway
+                new_logs.append(highlight_rules_func(line.rstrip('\n')))  # add the highlighted log to the list and strip any newline at the end
+            last_position = f.tell()  # get the last position
+        new_logs_as_string = "<br>".join(new_logs)  # joining with <br> directly
+    except FileNotFoundError:
+        new_logs_as_string = ""
+        last_position = 0
+    return {"logs": new_logs_as_string, "last_position": last_position}  # also return the last position
+
+
+@router.get("/show_logs/{minutes}", response_class=HTMLResponse)
+def show_logs(minutes: int = 5):
+    # read the entire log file and generate HTML with logs up to `minutes` minutes from now
+    with open("opennode_fastapi_log.txt", "r") as f:
+        lines = f.readlines()
+    logs = []
+    now = datetime.now(timezone('UTC'))  # get current time, make it timezone-aware
+    for line in lines:
+        if line.strip() == "":
+            continue
+        if line[0].isdigit():
+            try:  # Try to parse the datetime
+                log_datetime_str = line.split(" - ")[0]  # assuming the datetime is at the start of each line
+                log_datetime = datetime.strptime(log_datetime_str, "%Y-%m-%d %H:%M:%S,%f")  # parse the datetime string to a datetime object
+                log_datetime = log_datetime.replace(tzinfo=timezone('UTC'))  # set the datetime object timezone to UTC to match `now`
+                if now - log_datetime <= timedelta(minutes=minutes):  # if the log is within `minutes` minutes from now
+                    continue  # ignore the log and continue with the next line
+            except ValueError:
+                pass  # If the line does not start with a datetime, ignore the ValueError and process the line anyway                        
+            logs.append(highlight_rules_func(line.rstrip('\n')))  # add the highlighted log to the list and strip any newline at the end
+    logs_as_string = "<br>".join(logs)  # joining with <br> directly
+    logs_as_string_newlines_rendered = logs_as_string.replace("\n", "<br>")
+    logs_as_string_newlines_rendered_font_specified = """
+    <html>
+    <head>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <script>
+    var logContainer;
+    var lastLogs = `{0}`;
+    var shouldScroll = true;
+    var userScroll = false;
+    var lastPosition = 0;
+    var minutes = {1};
+    function fetchLogs() {{
+        if (typeof minutes !== 'undefined' && typeof lastPosition !== 'undefined') {{
+            fetch('/show_logs_incremental/' + minutes + '/' + lastPosition)
+            .then(response => response.json())
+            .then(data => {{
+                if (logContainer) {{
+                    var div = document.createElement('div');
+                    div.innerHTML = data.logs;
+                    if (div.innerHTML) {{
+                        lastLogs += div.innerHTML;
+                        lastPosition = data.last_position;
+                    }}
+                    logContainer.innerHTML = lastLogs;
+                    if (shouldScroll) {{
+                        logContainer.scrollTop = logContainer.scrollHeight;
+                    }}
+                }}
+            }});
+        }}
+    }}
+    function checkScroll() {{
+        if(logContainer.scrollTop + logContainer.clientHeight < logContainer.scrollHeight) {{
+            userScroll = true;
+            shouldScroll = false;
+        }} else {{
+            userScroll = false;
+        }}
+        if (!userScroll) {{
+            // If the user is not manually scrolling, re-enable auto-scrolling after a delay
+            setTimeout(function(){{ shouldScroll = true; }}, 10000);
+        }}
+    }}
+    window.onload = function() {{
+        let p = document.getElementsByTagName('p');
+        for(let i = 0; i < p.length; i++) {{
+            let color = window.getComputedStyle(p[i]).getPropertyValue('color');
+            p[i].style.textShadow = `0 0 5px ${{color}}, 0 0 10px ${{color}}, 0 0 15px ${{color}}, 0 0 20px ${{color}}`;
+        }}
+        document.querySelector('#copy-button').addEventListener('click', function() {{
+            var text = document.querySelector('#log-container').innerText;
+            navigator.clipboard.writeText(text).then(function() {{
+                console.log('Copying to clipboard was successful!');
+            }}, function(err) {{
+                console.error('Could not copy text: ', err);
+            }});
+        }});
+        document.querySelector('#download-button').addEventListener('click', function() {{
+            var text = document.querySelector('#log-container').innerText;
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            element.setAttribute('download', 'pastel_gateway_verification_monitor_log__' + new Date().toISOString() + '.txt');
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+        }});
+    }}
+    document.addEventListener('DOMContentLoaded', (event) => {{
+        logContainer = document.getElementById('log-container');
+        logContainer.innerHTML = lastLogs;
+        logContainer.addEventListener('scroll', checkScroll);
+        fetchLogs();
+        setInterval(fetchLogs, 1000);  // Fetch the logs every 1 second
+    }});
+    </script>
+    </head>        
+    <style>
+    .log-container {{
+        scroll-behavior: smooth;
+        background-color: #2b2b2b; /* dark gray */
+        color: #d3d3d3; /* light gray */
+        background-image: linear-gradient(rgba(0,0,0,0.1) 1px, transparent 1px), linear-gradient(90deg, rgba(0,0,0,0.1) 1px, transparent 1px);
+        background-size: 100% 10px, 10px 100%;
+        background-position: 0 0, 0 0;
+        animation: scan 1s linear infinite;
+        @keyframes scan {{
+            0% {{
+                background-position: 0 0, 0 0;
+            }}
+            100% {{
+                background-position: -10px -10px, -10px -10px;
+            }}
+        }}
+        font-size: 14px;
+        font-family: monospace;
+        padding: 10px;
+        height: 100vh;
+        margin: 0;
+        box-sizing: border-box;
+        overflow: auto;
+    }}
+    .icon-button {{
+        position: fixed;
+        right: 10px;
+        margin: 10px;
+        background-color: #555;
+        color: white;
+        border: none;
+        cursor: pointer;
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        font-size: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+    }}
+    #copy-button {{
+        bottom: 80px;  // Adjust this value as needed
+    }}
+    #download-button {{
+        bottom: 10px;
+    }}
+    </style>
+    <body>
+    <pre id="log-container" class="log-container"></pre>
+    <button id="copy-button" class="icon-button"><i class="fas fa-copy"></i></button>
+    <button id="download-button" class="icon-button"><i class="fas fa-download"></i></button>
+    </body>
+    </html>""".format(logs_as_string_newlines_rendered, minutes)
+    return logs_as_string_newlines_rendered_font_specified
+
+
+@router.get("/show_logs", response_class=HTMLResponse)
+def show_logs_default():
+    return show_logs(5)
