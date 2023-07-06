a1550919822177d382cafc734a681494df0d5c56
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-07-06 16:46:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ from views import opennode_fastapi
 from fastapi.middleware.cors import CORSMiddleware
 from services import opennode_fastapi_service
 from multiprocessing import Process, freeze_support
-from pathos.pools import ProcessPool
 from pydantic import BaseModel
 from fastapi import BackgroundTasks
 import nest_asyncio
 nest_asyncio.apply()
 import asyncio
 import time
+
+from fastapi import Request
+import logging
+from typing import Callable
+
 
 
 description_string = """
@@ -38,60 +42,44 @@ app.add_middleware(
 )
 
 
-async def run_task_periodically(background_tasks: BackgroundTasks):
+@app.middleware("http")
+async def suppress_logging_middleware(request: Request, call_next: Callable):
+    if request.method == "GET":
+        # Get the current log level
+        current_log_level = logging.getLogger('uvicorn.access').getEffectiveLevel()
+        # Set the log level to WARNING, so it will only log WARNING, ERROR and CRITICAL
+        logging.getLogger('uvicorn.access').setLevel(logging.WARNING)
+        response = await call_next(request)
+        # Reset the log level to its original value
+        logging.getLogger('uvicorn.access').setLevel(current_log_level)
+    else:
+        response = await call_next(request)
+    return response
+
+
+async def run_task_periodically():
     while True:
-        return_message = await opennode_fastapi_service.run_populate_database_with_all_dd_service_data_func(background_tasks)
-        # return_message2 = await opennode_fastapi_service.run_scan_new_blocks_func(background_tasks)
+        return_message = await opennode_fastapi_service.run_populate_database_with_all_dd_service_data_func()
+        # return_message2 = await opennode_fastapi_service.run_scan_new_blocks_func()
         await asyncio.sleep(30)
 
 
-async def process_db_queue(background_tasks: BackgroundTasks):
+async def process_db_queue():
     while True:
         await db_write_queue.join()
         await asyncio.sleep(1)  # sleep for a while before checking the queue again
     
     
-def start_background_tasks(app, background_tasks: BackgroundTasks):
-    asyncio.create_task(opennode_fastapi_service.startup_cascade_file_download_lock_cleanup_func(background_tasks))
-    asyncio.create_task(process_db_queue(background_tasks))
-    asyncio.create_task(run_task_periodically(background_tasks))
+@app.on_event("startup")
+async def start_background_tasks():
+    asyncio.create_task(opennode_fastapi_service.startup_cascade_file_download_lock_cleanup_func())
+    asyncio.create_task(process_db_queue())
+    asyncio.create_task(run_task_periodically())
     
-
-class LogConfig(BaseModel):
-    """Logging configuration to be set for the server"""
-
-    LOGGER_NAME: str = "opennode_fastapi"
-    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
-    LOG_LEVEL: str = "DEBUG"
-
-    # Logging config
-    version = 1
-    disable_existing_loggers = False
-    formatters = {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": LOG_FORMAT,
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    }
-    handlers = {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
-        },
-    }
-    loggers = {
-        "opennode_fastapi": {"handlers": ["default"], "level": LOG_LEVEL},
-    }
-
 
 def main():
     configure(dev_mode=True)
-    # noinspection PyTypeChecker
     #uvicorn.run(app, host='127.0.0.1', port=8002, debug=True)
-    log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
-    log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
 
 
 def configure(dev_mode: bool):
@@ -112,5 +100,4 @@ if __name__ == '__main__':
     freeze_support()    
     main()
 else:
-    start_background_tasks(app, background_tasks)
     configure(dev_mode=False)
