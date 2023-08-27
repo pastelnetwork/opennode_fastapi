78574cc8706e5f36fa5c1715e94bfa5c011b72e7
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-27 19:35:28 -0400

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -2,13 +2,15 @@ from pathlib import Path
 from typing import Callable, Optional
 import asyncio
 import time
-
+import logging
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from data.modelbase import SqlAlchemyBase
+
+log = logging.getLogger("PastelOpenNodeFastAPI")
 
 # Exponential backoff parameters
 initial_delay = 0.1  # Initial delay in seconds
@@ -26,46 +28,32 @@ last_write_time = time.time()
 
 def global_init(db_file: str):
     global __factory, __async_engine
-
     if __factory:
         return
-
     if not db_file or not db_file.strip():
         raise Exception("You must specify a db file.")
-
     folder = Path(db_file).parent
     folder.mkdir(parents=True, exist_ok=True)
-
-    # Post-recording update:
-    # SQLAlchemy started enforcing the underlying Python DB API was truly async
-    # We don't really get that with SQLite but when you switch something like Postgres
-    # It would "light up" with async. Since recording, SQLAlchemy throws and error
-    # if this would be the case. We need to explicitly switch to aiosqlite as below.
     conn_str = 'sqlite+pysqlite:///' + db_file.strip() + '?timeout=15'
     async_conn_str = 'sqlite+aiosqlite:///' + db_file.strip() + '?timeout=15'
     engine = sa.create_engine(conn_str, echo=False, connect_args={"check_same_thread": False})
     __async_engine = create_async_engine(async_conn_str, echo=False, connect_args={"check_same_thread": False})
     with engine.begin() as connection:
         connection.execute(text("PRAGMA journal_mode=WAL;"))
-        connection.execute(text("PRAGMA synchronous=NORMAL;"))  # Adjust as needed
-        connection.execute(text("PRAGMA cache_size=-20000;"))  # Use 20 MB cache. Adjust as needed
+        connection.execute(text("PRAGMA synchronous=NORMAL;"))
+        connection.execute(text("PRAGMA cache_size=-524288;"))
+        connection.execute(text("PRAGMA busy_timeout=5000;"))  # 5 second timeout. Adjust as needed
         connection.execute(text("PRAGMA journal_size_limit=5242880;"))  # Set a 5 MB limit. Adjust as needed
     __factory = orm.sessionmaker(bind=engine)
-    # noinspection PyUnresolvedReferences
-    import data.__all_models
-    # print("All SQLAlchemy models that are currently loaded:")
-    # print(SqlAlchemyBase.metadata.tables.keys())
+    import data.__all_models  # noqa: F401
     SqlAlchemyBase.metadata.create_all(engine)
 
-
 def create_session() -> Session:
-    global __factory
     if not __factory:
         raise Exception("You must call global_init() before using this method.")
     session: Session = __factory()
     session.expire_on_commit = False
     return session
-
 
 def create_async_session() -> AsyncSession:
     global __async_engine
@@ -74,7 +62,6 @@ def create_async_session() -> AsyncSession:
     session: AsyncSession = AsyncSession(__async_engine)
     session.sync_session.expire_on_commit = False
     return session
-
 
 async def wal_checkpoint_func():
     global last_write_time
@@ -85,40 +72,37 @@ async def wal_checkpoint_func():
             try:
                 async with __async_engine.begin() as connection:
                     result = await connection.execute('PRAGMA wal_checkpoint;')
-                    logger.info(f"WAL checkpoint result: {result.fetchone()}")
+                    log.info(f"WAL checkpoint result: {result.fetchone()}")
                     retry_count = 0  # Reset the retry count if the operation was successful
             except Exception as e:
                 retry_count += 1
                 backoff_time = min(max_delay, initial_delay * (backoff_factor ** retry_count))
-                logger.error(f"Error occurred during WAL checkpoint: {e}, retrying in {backoff_time} seconds...")
+                log.error(f"Error occurred during WAL checkpoint: {e}, retrying in {backoff_time} seconds...")
                 await asyncio.sleep(backoff_time)
-                                
                 
 async def write_records_to_db_func():
     global last_write_time
     while True:
-        queue_size = db_write_queue.qsize()
-        logger.info(f"Queue size: {queue_size}")
         record, retry_count = await db_write_queue.get()
-        try:
+        async with create_async_session() as session:
             try:
-                async with create_async_session() as session:
+                async with session.begin():
                     session.add(record)
-                    await session.commit()
+                await session.commit()
                 last_write_time = time.time()
-            except:
-                async with create_async_session() as session:
-                    session.merge(record)
-                    await session.commit()
-                last_write_time = time.time()
-        except Exception as e:
-            # If an error occurs, requeue the record with an increased retry count
-            logger.error(f"Error occurred during DB write: {e}, retrying...")
-            await asyncio.sleep(min(max_delay, initial_delay * (backoff_factor ** retry_count)))
-            await db_write_queue.put((record, retry_count + 1))
-        else:
-            db_write_queue.task_done()
-
+            except (orm.exc.FlushError, sa.exc.IntegrityError) as e:
+                await session.rollback()
+                log.error(f"Error during DB write: {e}. Not retrying.")
+            except Exception as e:
+                await session.rollback()
+                log.error(f"Error occurred during DB write: {e}, retrying...")
+                await asyncio.sleep(min(max_delay, initial_delay * (backoff_factor ** retry_count)))
+                await db_write_queue.put((record, retry_count + 1))
+            else:
+                db_write_queue.task_done()
 
 async def add_record_to_write_queue(record):
+    if not isinstance(record, SqlAlchemyBase):
+        log.error(f"Invalid record type: {type(record)}. Expected an instance of a SQLAlchemy mapped class.")
+        return
     await db_write_queue.put((record, 0))
