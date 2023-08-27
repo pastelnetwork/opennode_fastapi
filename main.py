167e5d04e6fedca2f7ac76411f8b9a7d78cbfe6f
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2023-08-27 19:32:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 from pathlib import Path
-
+import asyncio
+from fastapi import Request
+import logging
+from typing import Callable
 import fastapi
-import fastapi_chameleon
-import uvicorn
-import random
-
 from starlette.staticfiles import StaticFiles
-
 from data import db_session
 from data.db_session import db_write_queue
 from views import opennode_fastapi
 from fastapi.middleware.cors import CORSMiddleware
 from services import opennode_fastapi_service
-from multiprocessing import Process, freeze_support
-from pydantic import BaseModel
+from multiprocessing import freeze_support
 from fastapi import BackgroundTasks
 import nest_asyncio
 nest_asyncio.apply()
-import asyncio
-import time
-
-from fastapi import Request
-import logging
-from typing import Callable
-
-
 
 description_string = """
 🎢 Pastel's OpenNode FastAPI provides various informational API endpoints to retrieve information about the Pastel Blockchain. 💸
@@ -40,7 +29,6 @@ app.add_middleware(
     allow_headers=["*"],
     allow_credentials=True,
 )
-
 
 @app.middleware("http")
 async def suppress_logging_middleware(request: Request, call_next: Callable):
@@ -59,8 +47,7 @@ async def suppress_logging_middleware(request: Request, call_next: Callable):
 
 async def run_task_periodically():
     while True:
-        return_message = await opennode_fastapi_service.run_populate_database_with_all_dd_service_data_func()
-        # return_message2 = await opennode_fastapi_service.run_scan_new_blocks_func()
+        _ = await opennode_fastapi_service.run_populate_database_with_all_dd_service_data_func()
         await asyncio.sleep(30)
 
 
@@ -73,7 +60,9 @@ async def process_db_queue():
 @app.on_event("startup")
 async def start_background_tasks():
     asyncio.create_task(opennode_fastapi_service.startup_cascade_file_download_lock_cleanup_func())
+    asyncio.create_task(opennode_fastapi_service.startup_dd_service_lock_cleanup_func())
     asyncio.create_task(process_db_queue())
+    asyncio.create_task(db_session.write_records_to_db_func())
     asyncio.create_task(run_task_periodically())
     
 
